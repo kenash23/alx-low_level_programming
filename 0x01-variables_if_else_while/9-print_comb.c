9fcf2ee6be549908f0fcc24a48dc6c27e39e7622
kenash23 <kenuashhana@gmail.com>
2022-04-08 11:14:05 -0500

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(48 + i);
+		if (c !=9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
