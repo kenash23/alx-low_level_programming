5f0f100ea47d68a1a312d89e995ba16061780df6
kenash23 <kenuashhana@gmail.com>
2022-04-08 11:03:45 -0500

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print numbers in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; ++x)
+		putchar(x);
+	for (x = 'a'; x <= 'f'; ++x)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
