66c8a3393da5c1a253988e58d2018ee47acd1789
kenash23 <kenuashhana@gmail.com>
2022-04-13 11:19:46 -0500

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * main - main
+ * main - tests the function that prints +ve or -ve.
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
+
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
