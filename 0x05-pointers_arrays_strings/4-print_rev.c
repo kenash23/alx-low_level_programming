9192d9ec447f6bef58e179362840dfb06508daea
kenash23 <kenuashhana@gmail.com>
2022-04-19 14:25:36 -0500

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a sting in a reverse order
+ * @s: pointer to an int that will be changed
+ * 
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
