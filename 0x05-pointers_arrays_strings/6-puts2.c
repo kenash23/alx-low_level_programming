d8bb0c8b97236eb9711c68eebb00309e08723e17
kenash23 <kenuashhana@gmail.com>
2022-04-19 14:51:52 -0500

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * _puts2 - prints evry character of a sting@str: a pointer to an int that will be changed
+ *
+ * Return: void
+ */
+void _puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] = '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar (str[a]);
+	}
+	_putchar('\n');
+}
