92bc590eb48d39179a3055a9c947032e400cb0b4
kenash23 <kenuashhana@gmail.com>
2022-04-19 14:56:08 -0500

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints evry other character of a sting
+ * @str: a pointer to an int that will be changed
+ *
+ * Return: void
+ */
+void puts-half(char *str)
+{
+	int i, last;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	last = (i + 1) /2;
+
+	for (i = last; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
