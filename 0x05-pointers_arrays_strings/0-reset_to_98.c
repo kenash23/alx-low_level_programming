74290005898384dc59bb4281762337a25d2eef74
kenash23 <kenuashhana@gmail.com>
2022-04-19 12:03:21 -0500

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates integer vaue to n
+ * @n: A pointer to an int that will be updated
+ *
+ * Return - void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
