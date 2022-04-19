67c4219f2a71e849d33de087adf08bc64d58e6bb
kenash23 <kenuashhana@gmail.com>
2022-04-19 14:05:58 -0500

    3

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,9 +2,11 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
- * Return: on Sucess
+ *
+ * Return: on Sucess 1
  * On error: -1 
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
