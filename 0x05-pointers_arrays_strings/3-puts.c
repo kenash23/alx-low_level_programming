d5e77c3c18e2820c264a586b3ed7ff6116766f4a
kenash23 <kenuashhana@gmail.com>
2022-04-19 13:07:37 -0500

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_puts - prints a sting
+*@str: a pointer to an int that will be changed
+*
+*Return: void
+*/
+void _puts(char *str)
+{
+	char *c;
+	int m;
+
+	c = str;
+	for (m = 0; c[m]; m++)
+	{
+		_putchar (c[m]);
+	}
+	_putchar('\n');
+}
