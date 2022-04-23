0f9dfbee9d506d16c8e587fdfa9fe8ac640f67e1
kenash23 <kenuashhana@gmail.com>
2022-04-23 07:36:30 -0700

    8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,7 +11,7 @@ char *rot13(char *s)
 	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (x = 0; s[x] != '\0': x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (y = 0; input[y] != '\0'; y++)
 		{
